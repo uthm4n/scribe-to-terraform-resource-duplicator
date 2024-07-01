22e20b615db19e604c7dcba4a82ec4f1cf965d43
Uthman Al-Ayek Eqbal <135247149+uthm4n@users.noreply.github.com>
2024-07-01 14:24:08 +0100

    Create terraformResourceBuilder.groovy

---
+++ terraformResourceBuilder.groovy
@@ -0,0 +1,45 @@
+import com.morpheusdata.core.util.HttpApiClient
+import groovy.json.JsonOutput
+import org.w3c.dom.css.Counter
+import com.morpheusdata.core.util.MorpheusUtils
+
+Map getTerraformConfig(int workflowId) {
+    def client = new HttpApiClient()
+    def requestOptions = new HttpApiClient.RequestOptions()
+    def apiToken = morpheus.apiAccessToken.size() == 36 ? morpheus.apiAccessToken.toString() : cypher.read('secret/morpheus-api-key')
+    requestOptions.headers = ["Authorization": "Bearer ${apiToken}"]
+    requestOptions.ignoreSSL = true
+
+    def terraformResource = [:]
+    try {
+        def response = client.callJsonApi("${morpheus.applianceUrl}", "/api/task-sets/${workflowId}", requestOptions, "GET")
+        if (response.success) {
+            def workflow = response.data?.taskSet
+            terraformResource.workflowOne = [
+                    "name": workflow.name + "-cloned",
+                    "labels": workflow.labels,
+                    "task_ids": workflow.tasks,
+                    "option_types": workflow.optionTypes.id
+            ]
+        }
+    } catch (Exception e) {
+        println "Failure occurred! ${e.printStackTrace()}"
+    }
+    return terraformResource
+}
+
+def terraformResource = getTerraformConfig(15) // Enter a workflow ID here
+
+String buildTerraformConfig(Map terraformResource) {
+    def workflow = terraformResource.workflowOne
+    return """
+resource "morpheus_operational_workflow" "${workflow.name}" {
+  name   = "${workflow.name}"
+  labels = ${JsonOutput.toJson(workflow.labels)}
+  task_ids = ${JsonOutput.toJson(workflow.task_ids)}
+  option_types = ${JsonOutput.toJson(workflow.option_types)}
+}
+"""
+}
+
+println buildTerraformConfig(terraformResource)
